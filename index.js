0180cd46357b18926e44b3f291163352f7ed4379
Dickson Amenyo <36271402+dkerobean@users.noreply.github.com>
2025-01-13 07:50:24 +0000

    connected to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db')
 const app = express();
-const port = 3000;
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Connect to the database
+connectDB();
+
+// Use the port from the .env file
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -11,6 +21,6 @@ app.get('/api', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
